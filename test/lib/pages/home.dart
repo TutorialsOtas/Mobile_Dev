af14ce946fae810027af16e641c0bb6c5862fc11
Okai Tettey-Antie Samuel <teesam615@gmail.com>
2025-12-06 04:48:56 +0000

    search-bar done'

--- test/lib/pages/home.dart
+++ test/lib/pages/home.dart
@@ -4,12 +4,47 @@ import 'package:flutter_svg/flutter_svg.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  get margin => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.transparent,
+                  hintText: 'Search...',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text(
-          'KudiBaabu',
+          'Meal Monkey',
           style: TextStyle(
             color: Colors.yellow,
             fontSize: 22,
@@ -19,33 +54,51 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.blueAccent,
         centerTitle: true,
         elevation: 4,
-        leading: Container(
-          margin: EdgeInsets.all(8),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: SvgPicture.asset(
-            'assets/icons/arrow.svg',
-            // ignore: deprecated_member_use
-            color: Colors.white,
-          ),
-        ),
-        actions: [
-          Container(
+        leading: GestureDetector(
+          onTap: () {
+            // Action when the back arrow is tapped
+          },
+          child: Container(
             margin: EdgeInsets.all(8),
             alignment: Alignment.center,
             decoration: BoxDecoration(
               color: Colors.black,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 4,
+                  offset: Offset(0, 2),
+                ),
+              ],
               borderRadius: BorderRadius.circular(12),
             ),
-            height: 40,
-            width: 40,
             child: SvgPicture.asset(
-              'assets/icons/3dots.svg',
+              'assets/icons/arrow.svg',
               // ignore: deprecated_member_use
               color: Colors.white,
+            ),
+          ),
+        ),
+
+        actions: [
+          GestureDetector(
+            onTap: () {
+              // Action when the 3 dots icon is tapped
+            },
+            child: Container(
+              margin: EdgeInsets.all(8),
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              height: 40,
+              width: 40,
+              child: SvgPicture.asset(
+                'assets/icons/3dots.svg',
+                // ignore: deprecated_member_use
+                color: Colors.white,
+              ),
             ),
           ),
         ],
