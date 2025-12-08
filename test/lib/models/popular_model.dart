92fb5f57ce45378d211a9cfe93323d4a78fb9ac8
Okai Tettey-Antie Samuel <teesam615@gmail.com>
2025-12-08 09:51:51 +0000

    about-to-clear-up-the-mess

--- test/lib/models/popular_model.dart
+++ test/lib/models/popular_model.dart
@@ -1,10 +1,1 @@
-import 'package:flutter/material.dart';
 
-class PopularModel extends StatelessWidget {
-  const PopularModel({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
