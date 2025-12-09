310b67f7fb510d0683f737899acb9d5b38fcf726
Okai Tettey-Antie Samuel <teesam615@gmail.com>
2025-12-09 07:11:49 +0000

    boutta-start

--- test2/lib/main.dart
+++ test2/lib/main.dart
@@ -1,20 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:test/pages/home.dart';
+//import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(fontFamily: 'Montserrat'),
-      home: const HomePage(),
-    );
-  }
-}
