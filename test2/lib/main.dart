b4e9fbde597813c0b976c9b9f336cad4a73f5dbd
Okai Tettey-Antie Samuel <teesam615@gmail.com>
2025-12-10 12:53:58 +0000

    introduction-1

--- test2/lib/main.dart
+++ test2/lib/main.dart
@@ -1,6 +1,26 @@
-//import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-void main(){
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( // <--- Opened 1
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      title: 'Flutter Demo - Introduction',
+      home: Scaffold( // <--- Opened 2
+        appBar: AppBar(
+          title: Text('Flutter Demo - Introduction'),
+        ),
+        body: Center(
+            child: Text('this is a starter. a skeleton to show that we have began the journey from lesson 1.')
+        ),
+      ),
+    ); 
+  }
 }
-
