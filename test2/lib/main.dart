392b529b6582fec20db952fb1f4f7a96e08034b1
Okai Tettey-Antie Samuel <teesam615@gmail.com>
2025-12-16 11:47:30 +0000

    introductory-step

--- test2/lib/main.dart
+++ test2/lib/main.dart
@@ -7,20 +7,42 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( // <--- Opened 1
+    return MaterialApp( // <--- The Theme is defined here
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.blue,
+        iconTheme: IconThemeData(
+            color: Colors.red
+        ),
+
+        textTheme: TextTheme(
+            displaySmall: TextStyle(
+              color: Colors.red, // This should now be applied
+              fontSize: 24, // Added for better visibility
+            )
+        ),
+
       ),
+
       title: 'Flutter Demo - Introduction',
-      home: Scaffold( // <--- Opened 2
-        appBar: AppBar(
-          title: Text('Flutter Demo - Introduction'),
-        ),
-        body: Center(
-            child: Text('this is a starter. a skeleton to show that we have began the journey from lesson 1.')
-        ),
+      // Use a Builder to get a context that is under MaterialApp
+      home: Builder(
+          builder: (context) { // <--- This 'context' can see the theme
+            return Scaffold(
+              appBar: AppBar(
+                leading: Icon(Icons.menu,),
+                title: Text('Flutter Demo - Introduction',),
+              ),
+
+              body: Center(
+                child: Text(
+                  'Flutter Demo - Introduction',
+                  style: Theme.of(context).textTheme.displaySmall, // Now this will work
+                ),
+              ),
+            );
+          }
       ),
-    ); 
+    );
   }
 }
